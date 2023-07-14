ab0ec559f9f6368e744430decef3d18110318f02
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-07-14 19:39:41 +1000

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
       process.env.DB_USER,
       process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: '3.25.181.87',
     dialect: 'mysql',
     port: 3306,
     dialectOptions: {
